6974085f940599bfbf90020f6d8c80cd18fa3b9d
dileepthomas <dileepthomas1993@gmail.com>
2017-12-27 21:30:16 +0530

    using debugger on the js file if we type c also it will stop at debugger where it is written

--- playground/debugging.js
+++ playground/debugging.js
@@ -2,6 +2,7 @@ let person = {
   name:"Dileep"
 }
 person.age = 24
-// after clicking on n , just type repl read evaluate print loop to see the values 
+// after clicking on n , just type repl read evaluate print loop to see the values
+debugger;
 person.name ="John"
 console.log(person);
