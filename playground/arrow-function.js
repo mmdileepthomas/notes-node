28482e879932811e6c4b1f149d754ade1f14d53d
dileepthomas <dileepthomas1993@gmail.com>
2017-12-28 21:46:35 +0530

    using normal arrow functions but the name not showing , how to overcome the issue in the below line sayHiAlt, When we use arrow function it will refer to the global this but name not available if we use instead of this as user.name in arrow function it will print

---
+++ playground/arrow-function.js
@@ -0,0 +1,15 @@
+let square = x => x * x
+console.log(square(4));
+
+let user ={
+  name:"Dileep Thomas",
+  sayHi: () => {
+    console.log(`hi , i m ${this.name}`);
+  },
+  sayHiAlt () {
+    console.log(`hi , i m ${this.name}`);
+  }
+}
+
+user.sayHi();
+user.sayHiAlt();
